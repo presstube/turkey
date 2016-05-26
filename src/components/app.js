10f84b46436af88b4bcb57b8a7a7e3d52ff0c055
James Paterson <j@presstube.com>
2016-05-26 11:44:41 -0400

    scaffolded a component

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+
+import React from "react"
+
+export default class App extends React.Component {
+  render () {
+    return (
+      <div>
+        Hello, world! I am an app. yay!!!
+      </div>
+    )
+  }
+}
