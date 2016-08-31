e27a20c63213fe955163f50da61afe52723b2921
Jason Kuhrt <jasonkuhrt@me.com>
2016-08-31 09:30:35 -0400

    Tidy

--- src/components/app.js
+++ src/components/app.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render () {
     return (
       <div>
-        Hello, world! I am an app. yay!!!
+        Dear World, Hello! Regards, App Component.
       </div>
     )
   }
--- src/main.js
+++ src/main.js
@@ -1,12 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
-
 import App from "./components/app"
 
+
+
 ReactDOM.render(<App />, document.getElementById("app"))
-
-
-const foo = <div />
-
-console.log(foo)
-
